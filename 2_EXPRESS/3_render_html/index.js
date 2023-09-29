9f6c7045120075acd71c21d689aa4c26bc61c2dd
AllyAwada <allyhawada@icloud.com>
2023-09-29 10:08:44 -0300

    render html com express

--- 2_EXPRESS/3_render_html/index.js
+++ 2_EXPRESS/3_render_html/index.js
@@ -1,13 +1,16 @@
 // Módulos interno, core modules e módulos externo
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+const caminho = path.join(__dirname, 'templates')
+
 // Rota de envio de informações para o html
 app.get('/', (requisição, resposta) => {
-    resposta.send("Seja bem-vindo(a) ao meu servidor")
-
+    resposta.sendFile(`${caminho}/index.html`)
 })
+
 
 
 // Monitonar uma porta do proprio computador
