825910289a92ab17eeb9c1cdf41191973d04ec66
AllyAwada <allyhawada@icloud.com>
2023-10-27 09:52:22 -0300

    nodemon

---
+++ 2_EXPRESS/4_nodemon/index.js
@@ -0,0 +1,21 @@
+// Módulos interno, core modules e módulos externo
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminho = path.join(__dirname, 'templates')
+
+// Rota de envio de informações para o html
+app.get('/', (requisição, resposta) => {
+    resposta.sendFile(`${caminho}/index.html`)
+})
+
+
+
+// Monitonar uma porta do proprio computador
+app.listen(3000, () => {
+    console.log(" Servidor rodando na porta 3000.")
+})
+
+// localhost:3000
